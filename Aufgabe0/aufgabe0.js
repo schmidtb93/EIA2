7547e0f0a59a8119f0c3e65ff70190e3825c7a79
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-10-07 20:20:27 +0200

    Add files via upload

---
+++ Aufgabe0/aufgabe0.js
@@ -0,0 +1,9 @@
+function begruessung() {
+    var person = prompt("Wie ist dein Name?");
+    if (person != null) {
+        document.getElementById("content").innerHTML =
+            "Hallo " + person + "! Wie geht es dir?";
+        console.log("Hallo " + person + "! Wie geht es dir?");
+    }
+}
+//# sourceMappingURL=aufgabe0.js.map

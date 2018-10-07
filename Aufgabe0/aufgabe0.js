fc1bbb4d51330f9b4fbba36919fcf925d604edba
Sebastian <Sebastian@SebastianLaptop.ghb.hs-furtwangen.de>
2018-10-07 11:09:42 +0200

    A

---
+++ Aufgabe0/aufgabe0.js
@@ -0,0 +1,9 @@
+function begruessung() {
+    var person = prompt("Wie hei�t du?");
+    if (person != null) {
+        document.getElementById("content").innerHTML;
+        "Hallo " + person + "! Wie geht es dir?";
+        console.log("Hallo " + person + "! Wie geht es dir?");
+    }
+}
+//# sourceMappingURL=aufgabe0.js.map
---
+++ Aufgabe0/aufgabe0.ts
@@ -0,0 +1,8 @@
+function begruessung() {
+    var person = prompt("Wie heiﬂt du?");
+    if (person != null) {
+document.getElementById("content").innerHTML 
+"Hallo " + person + "! Wie geht es dir?"
+console.log("Hallo " + person + "! Wie geht es dir?");
+    }
+}

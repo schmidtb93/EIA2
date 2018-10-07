c5eb504b4f24c03795adb91772b6d29177ed8bcd
Sebastian <Sebastian@SebastianLaptop.ghb.hs-furtwangen.de>
2018-10-07 17:21:42 +0200

    A

--- Aufgabe0/aufgabe0.js
+++ Aufgabe0/aufgabe0.js
@@ -1,8 +1,8 @@
 function begruessung() {
     var person = prompt("Wie ist dein Name?");
     if (person != null) {
-        document.getElementById("content").innerHTML;
-        "Hallo " + person + "! Wie geht es dir?";
+        document.getElementById("content").innerHTML =
+            "Hallo " + person + "! Wie geht es dir?";
         console.log("Hallo " + person + "! Wie geht es dir?");
     }
 }

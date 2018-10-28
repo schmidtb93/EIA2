cf2d99b66e20d80bea4f82faea5495608e1032b3
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-10-28 09:32:08 +0100

    Aufgabe 2.5 aka Uno Redesign - 28.10.2018

---
+++ Aufgabe 2.5/Aufgabe2.5.ts
@@ -0,0 +1,492 @@
+//Aufgabe: Aufgabe 2.5
+//Name: Sebastian Schmidt
+//Matrikel: 259526
+//Datum:28.10.2018
+//Hiermit versichere ich, dass ich diesen
+//Code selbst geschrieben habe. Er wurde
+//nicht kopiert und auch nicht diktiert.
+
+namespace unoneu {
+    interface card {
+        color: string;
+        value: string;
+    }
+
+    let karte0: card = {
+        color: "red",
+        value: "0",
+    }
+    let karte1: card = {
+        color: "blue",
+        value: "0",
+    }
+    let karte2: card = {
+        color: "yellow",
+        value: "0",
+    }
+    let karte3: card = {
+        color: "green",
+        value: "0",
+    }
+    let karte4: card = {
+        color: "red",
+        value: "1",
+    }
+    let karte5: card = {
+        color: "blue",
+        value: "1",
+    }
+    let karte6: card = {
+        color: "yellow",
+        value: "1",
+    }
+    let karte7: card = {
+        color: "green",
+        value: "1",
+    }
+    let karte8: card = {
+        color: "red",
+        value: "1",
+    }
+    let karte9: card = {
+        color: "blue",
+        value: "1",
+    }
+    let karte10: card = {
+        color: "yellow",
+        value: "1",
+    }
+    let karte11: card = {
+        color: "green",
+        value: "1",
+    }
+    let karte12: card = {
+        color: "red",
+        value: "2",
+    }
+    let karte13: card = {
+        color: "blue",
+        value: "2",
+    }
+    let karte14: card = {
+        color: "yellow",
+        value: "2",
+    }
+    let karte15: card = {
+        color: "green",
+        value: "2",
+    }
+    let karte16: card = {
+        color: "red",
+        value: "2",
+    }
+    let karte17: card = {
+        color: "blue",
+        value: "2",
+    }
+    let karte18: card = {
+        color: "yellow",
+        value: "2",
+    }
+    let karte19: card = {
+        color: "green",
+        value: "2",
+    }
+    let karte20: card = {
+        color: "red",
+        value: "3",
+    }
+    let karte21: card = {
+        color: "blue",
+        value: "3",
+    }
+    let karte22: card = {
+        color: "yellow",
+        value: "3",
+    }
+    let karte23: card = {
+        color: "green",
+        value: "3",
+    }
+    let karte24: card = {
+        color: "red",
+        value: "3",
+    }
+    let karte25: card = {
+        color: "blue",
+        value: "3",
+    }
+    let karte26: card = {
+        color: "yellow",
+        value: "3",
+    }
+    let karte27: card = {
+        color: "green",
+        value: "3",
+    }
+
+    let karte28: card = {
+        color: "red",
+        value: "4",
+    }
+    let karte29: card = {
+        color: "blue",
+        value: "4",
+    }
+    let karte30: card = {
+        color: "yellow",
+        value: "4",
+    }
+    let karte31: card = {
+        color: "green",
+        value: "4",
+    }
+    let karte32: card = {
+        color: "red",
+        value: "4",
+    }
+    let karte33: card = {
+        color: "blue",
+        value: "4",
+    }
+    let karte34: card = {
+        color: "yellow",
+        value: "4",
+    }
+    let karte35: card = {
+        color: "green",
+        value: "4",
+    }
+    let karte36: card = {
+        color: "red",
+        value: "5",
+    }
+    let karte37: card = {
+        color: "blue",
+        value: "5",
+    }
+    let karte38: card = {
+        color: "yellow",
+        value: "5",
+    }
+    let karte39: card = {
+        color: "green",
+        value: "5",
+    }
+    let karte40: card = {
+        color: "red",
+        value: "5",
+    }
+    let karte41: card = {
+        color: "blue",
+        value: "5",
+    }
+    let karte42: card = {
+        color: "yellow",
+        value: "5",
+    }
+    let karte43: card = {
+        color: "green",
+        value: "5",
+    }
+    let karte44: card = {
+        color: "red",
+        value: "6",
+    }
+    let karte45: card = {
+        color: "blue",
+        value: "6",
+    }
+    let karte46: card = {
+        color: "yellow",
+        value: "6",
+    }
+    let karte47: card = {
+        color: "green",
+        value: "6",
+    }
+    let karte48: card = {
+        color: "red",
+        value: "6",
+    }
+    let karte49: card = {
+        color: "blue",
+        value: "6",
+    }
+    let karte50: card = {
+        color: "yellow",
+        value: "6",
+    }
+    let karte51: card = {
+        color: "green",
+        value: "6",
+    }
+    let karte52: card = {
+        color: "red",
+        value: "7",
+    }
+    let karte53: card = {
+        color: "blue",
+        value: "7",
+    }
+    let karte54: card = {
+        color: "yellow",
+        value: "7",
+    }
+    let karte55: card = {
+        color: "green",
+        value: "7",
+    }
+    let karte56: card = {
+        color: "red",
+        value: "7",
+    }
+    let karte57: card = {
+        color: "blue",
+        value: "7",
+    }
+    let karte58: card = {
+        color: "yellow",
+        value: "7",
+    }
+    let karte59: card = {
+        color: "green",
+        value: "7",
+    }
+    let karte60: card = {
+        color: "red",
+        value: "8",
+    }
+    let karte61: card = {
+        color: "blue",
+        value: "8",
+    }
+    let karte62: card = {
+        color: "yellow",
+        value: "8",
+    }
+    let karte63: card = {
+        color: "green",
+        value: "8",
+    }
+    let karte64: card = {
+        color: "red",
+        value: "8",
+    }
+    let karte65: card = {
+        color: "blue",
+        value: "8",
+    }
+    let karte66: card = {
+        color: "yellow",
+        value: "8",
+    }
+    let karte67: card = {
+        color: "green",
+        value: "8",
+    }
+    let karte68: card = {
+        color: "red",
+        value: "9",
+    }
+    let karte69: card = {
+        color: "blue",
+        value: "9",
+    }
+    let karte70: card = {
+        color: "yellow",
+        value: "9",
+    }
+    let karte71: card = {
+        color: "green",
+        value: "9",
+    }
+    let karte72: card = {
+        color: "red",
+        value: "9",
+    }
+    let karte73: card = {
+        color: "blue",
+        value: "9",
+    }
+    let karte74: card = {
+        color: "yellow",
+        value: "9",
+    }
+    let karte75: card = {
+        color: "green",
+        value: "9",
+    }
+    let karte76: card = {
+        color: "red",
+        value: "+2",
+    }
+    let karte77: card = {
+        color: "blue",
+        value: "+2",
+    }
+    let karte78: card = {
+        color: "yellow",
+        value: "+2",
+    }
+    let karte79: card = {
+        color: "green",
+        value: "+2",
+    }
+    let karte80: card = {
+        color: "red",
+        value: "+2",
+    }
+    let karte81: card = {
+        color: "blue",
+        value: "+2",
+    }
+    let karte82: card = {
+        color: "yellow",
+        value: "+2",
+    }
+    let karte83: card = {
+        color: "green",
+        value: "+2",
+    }
+    let karte84: card = {
+        color: "red",
+        value: "skip",
+    }
+    let karte85: card = {
+        color: "blue",
+        value: "skip",
+    }
+    let karte86: card = {
+        color: "yellow",
+        value: "skip",
+    }
+    let karte87: card = {
+        color: "green",
+        value: "skip",
+    }
+    let karte88: card = {
+        color: "red",
+        value: "skip",
+    }
+    let karte89: card = {
+        color: "blue",
+        value: "skip",
+    }
+    let karte90: card = {
+        color: "yellow",
+        value: "skip",
+    }
+    let karte91: card = {
+        color: "green",
+        value: "skip",
+    }
+    let karte92: card = {
+        color: "red",
+        value: "change direction",
+    }
+    let karte93: card = {
+        color: "blue",
+        value: "change direction",
+    }
+    let karte94: card = {
+        color: "yellow",
+        value: "change direction",
+    }
+    let karte95: card = {
+        color: "green",
+        value: "change direction",
+    }
+    let karte96: card = {
+        color: "red",
+        value: "change direction",
+    }
+    let karte97: card = {
+        color: "blue",
+        value: "change direction",
+    }
+    let karte98: card = {
+        color: "yellow",
+        value: "change direction",
+    }
+    let karte99: card = {
+        color: "green",
+        value: "change direction",
+    }
+    let karte100: card = {
+        color: "black",
+        value: "+2",
+    }
+    let karte101: card = {
+        color: "black",
+        value: "+2",
+    }
+    let karte102: card = {
+        color: "black",
+        value: "+2",
+    }
+    let karte103: card = {
+        color: "black",
+        value: "+2",
+    }
+    let karte104: card = {
+        color: "black",
+        value: "+4",
+    }
+    let karte105: card = {
+        color: "black",
+        value: "+4",
+    }
+    let karte106: card = {
+        color: "black",
+        value: "+4",
+    }
+    let karte107: card = {
+        color: "black",
+        value: "+4",
+    }
+
+    //Alle 108 Karten im Array
+    let allCards: card[] = [karte0, karte1, karte2, karte3, karte4, karte5, karte6, karte7, karte8, karte9, karte10, karte11, karte12, karte13, karte14, karte15, karte16, karte17, karte18, karte19, karte20, karte21, karte22, karte23, karte24, karte25, karte26, karte27, karte28, karte29, karte30, karte31, karte32, karte33, karte34, karte35, karte36, karte37, karte38, karte39, karte40, karte41, karte42, karte43, karte44, karte45, karte46, karte47, karte48, karte49, karte50, karte51, karte52, karte53, karte54, karte55, karte56, karte57, karte58, karte59, karte60, karte61, karte62, karte63, karte64, karte65, karte66, karte67, karte68, karte69, karte70, karte71, karte72, karte73, karte74, karte75, karte76, karte77, karte78, karte79, karte80, karte81, karte82, karte83, karte84, karte85, karte86, karte87, karte88, karte89, karte90, karte91, karte92, karte93, karte94, karte95, karte96, karte97, karte98, karte99, karte100, karte101, karte102, karte103, karte104, karte105, karte106, karte107];
+
+    //PlaceDiv von der Ursprünglichen Aufgabe übernommen
+    function placeDiv(_color: string, _x: string, _n: number): void {
+        let div: HTMLDivElement = document.createElement("div");
+        document.body.appendChild(div);
+        div.setAttribute("id", "a" + _n);
+        document.getElementById("a" + _n).innerHTML += _x;
+        let s: CSSStyleDeclaration = div.style;
+        s.border = "thin solid white";
+        s.textAlign = "center";
+        s.position = "absolute";
+        s.backgroundColor = _color;
+        s.width = 100 + "px";
+        s.height = 170 + "px";
+        s.left = (_n + 1) * 100 + "px";
+        s.right = (_n + 1) * 100 + "px";
+        s.bottom = 40 + "px";
+        s.borderRadius = 5 + "px";
+        s.color = "white";
+    }
+
+    function main(): void {
+        //Prompt von der ursprünglichen Aufgabe übernommen. ParseInt wäre besser.
+        let anzahlKarten: number;
+        let i: string = prompt("Bitte die Anzahl der Karten eingeben:");
+        anzahlKarten = Number(i);
+        
+        for (let i: number = 0; i < anzahlKarten; i++) {
+            
+            let randomCardnum: number = random(allCards.length);
+            placeDiv(allCards[randomCardnum].color, allCards[randomCardnum].value, i);
+            allCards.splice(randomCardnum, 1); //Zufällig gewähltes Array wird aus dem allCards Array entfernt
+            continue;
+        }
+    }
+    function random(_math: number): number {
+        return Math.floor(Math.random() * Math.floor(_math));
+    }
+
+    document.addEventListener(
+        "DOMContentLoaded", main);
+} 

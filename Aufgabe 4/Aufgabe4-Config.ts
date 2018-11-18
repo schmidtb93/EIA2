5e1add412d729f4b052f4f13e287e917efc5ee4e
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-11-18 19:13:55 +0100

    18.11.2018

--- Aufgabe 4/Aufgabe4-Config.ts
+++ Aufgabe 4/Aufgabe4-Config.ts
@@ -16,6 +16,7 @@ namespace aufgabe4 {
         document.getElementsByTagName("fieldset")[0].addEventListener("change", handleChange);
         document.getElementById("verify").addEventListener("click", verify);
     }
+
     //Funktion von Prof. Dell'Oro-Friedl und Aufgabe 13 EIA1
 
     function fillFieldset(): void {

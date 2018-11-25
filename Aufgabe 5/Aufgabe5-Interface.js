ec45feda8986f41586c7412305fed289ef563681
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-11-25 17:20:25 +0100

    Aufgabe 5 - 17.20 uhr // 25.11.2018

---
+++ Aufgabe 5/Aufgabe5-Interface.js
@@ -0,0 +1,10 @@
+var aufgabe5;
+(function (aufgabe5) {
+    aufgabe5.christmasBulb = [{ name: "Rot (2 Euro)", price: 2 }, { name: "Gold (3 Euro)", price: 3 }, { name: "Lila (2 Euro)", price: 2 }];
+    aufgabe5.candle = [{ name: "Kerzen rot (1 Euro)", price: 1 }, { name: "Kerzen gold (3 Euro)", price: 3 }, { name: "Kerzen silber (2 Euro)", price: 2 }];
+    aufgabe5.lametta = [{ name: "Gold Packung (5 Euro)", price: 5 }, { name: "Silber (3 Euro)", price: 3 }, { name: "Lila (3 Euro)", price: 3 }];
+    aufgabe5.treeTyp = [{ name: "Nordmannstanne (100 Euro)", price: 100 }, { name: "Plastiktanne (20 Euro)", price: 20 }, { name: "Fichte (50 Euro)", price: 50 }];
+    aufgabe5.holder = [{ name: "Mittel (Viereckig, 20 Euro)", price: 20 }, { name: "Klein (Rund, 10 Euro)", price: 10 }, { name: "Mittel (Viereckig, 15 Euro)", price: 15 }];
+    aufgabe5.shipment = [{ name: "DHL (15 Euro)", price: 15 }, { name: "UPS (20 Euro)", price: 20 }, { name: "Hermes (13 Euro)", price: 13 }];
+})(aufgabe5 || (aufgabe5 = {}));
+//# sourceMappingURL=Aufgabe5-Interface.js.map

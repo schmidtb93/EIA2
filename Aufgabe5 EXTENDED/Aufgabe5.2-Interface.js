093ae415819b5379f87bfdfc8416146a400f326e
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-11-25 17:03:33 +0100

    Aufgabe 5 Extended

---
+++ Aufgabe5 EXTENDED/Aufgabe5.2-Interface.js
@@ -0,0 +1,10 @@
+var aufgabe5extended;
+(function (aufgabe5extended) {
+    aufgabe5extended.christmasBulb = [{ name: "Rot (2 Euro)", price: 2 }, { name: "Gold (3 Euro)", price: 3 }, { name: "Lila (2 Euro)", price: 2 }];
+    aufgabe5extended.candle = [{ name: "Kerzen rot (1 Euro)", price: 1 }, { name: "Kerzen gold (3 Euro)", price: 3 }, { name: "Kerzen silber (2 Euro)", price: 2 }];
+    aufgabe5extended.lametta = [{ name: "Gold Packung (5 Euro)", price: 5 }, { name: "Silber (3 Euro)", price: 3 }, { name: "Lila (3 Euro)", price: 3 }];
+    aufgabe5extended.treeTyp = [{ name: "Nordmannstanne (100 Euro)", price: 100 }, { name: "Plastiktanne (20 Euro)", price: 20 }, { name: "Fichte (50 Euro)", price: 50 }];
+    aufgabe5extended.holder = [{ name: "Mittel (Viereckig, 20 Euro)", price: 20 }, { name: "Klein (Rund, 10 Euro)", price: 10 }, { name: "Mittel (Viereckig, 15 Euro)", price: 15 }];
+    aufgabe5extended.shipment = [{ name: "DHL (15 Euro)", price: 15 }, { name: "UPS (20 Euro)", price: 20 }, { name: "Hermes (13 Euro)", price: 13 }];
+})(aufgabe5extended || (aufgabe5extended = {}));
+//# sourceMappingURL=Aufgabe5.2-Interface.js.map

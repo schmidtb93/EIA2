46827e28f0d27bc67caf93754267ab864e7b216a
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-12-05 22:04:07 +0100

    test3

--- Aufgabe6/Server/Server.js
+++ Aufgabe6/Server/Server.js
@@ -41,9 +41,6 @@ var aufgabe6;
                     console.log(HTML);
                 }
             }
-            alleArtikel.push(HTML);
-            alleArtikel.push("<p>");
-            alleArtikel.push("<p>");
             for (let i = 0; i < alleArtikel.length; i++) {
                 _response.write(alleArtikel[i]); // Sendet eine Antwort an die angefragte URL
             }

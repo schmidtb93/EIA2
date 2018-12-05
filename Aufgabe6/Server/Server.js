e5023817ab4ef2dd0345ed6814d13d29a1a06429
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-12-05 21:29:17 +0100

    Test 1

--- Aufgabe6/Server/Server.js
+++ Aufgabe6/Server/Server.js
@@ -1,30 +1,45 @@
 "use strict";
-//Import die gesamte node.d.ts Datei und greift auf "declare module "http"" zu
 const Http = require("http");
+const Url = require("url");
 var L06_SendData;
 (function (L06_SendData) {
-    //Konsole gibt "Starting server" aus
     console.log("Starting server");
-    //process bezieht sich auf NodeJS.Heroku ist die angegebene Umgebung. Wenn Port nicht angegeben ist, wird Port 8100 benutzt 
     let port = process.env.PORT;
     if (port == undefined)
         port = 8100;
-    //Erlaubt den Transfer Hyper Text Transfer Protocol. 
-    let server = Http.createServer(); //Der PC fungiert als Server.
-    server.addListener("request", handleRequest); //Kommunikation zwischen Client und Server. Der Server sendet eine Anfrage "handleRequest".
-    server.addListener("listening", handleListen); //Listener wartet auf eingehede Daten
-    server.listen(port); //H�rt auf den Port 8100 
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
     function handleListen() {
-        console.log("Listening"); //Konsole gibt "Listening" aus
+        console.log("Listening");
     }
     function handleRequest(_request, _response) {
-        console.log(_request.url); //Konsole gibt "I hear voices!" aus //Gibt die Url zur�ck
-        //Ver�ndert die Werte von setHeader
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Bestimmt den Zeichencode sowie das folgendes Dokument ein Text ist 
-        _response.setHeader("Access-Control-Allow-Origin", "*"); //Sch�tzt Skriptsprachen-zugriff von fremden Quellen und erlaubt nur die eigene Ressource zu laden (Origin w�re das Protokoll, sowie die Dom�ne und der Port z.B https://eia2-nodetest.herokuapp.com mit dem Port 8100)  
-        _response.write(_request.url); // Sendet eine Antwort an die angefragte URL  (
-        _response.end(); //Der Inhalt von Response wird vom Client gesendet und es wird signalisiert das jene Anfrage vollst�ndig gesendet wurde
-        //Server wird mit strg+c beendet
+        console.log(_request.url);
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        let articles = [];
+        let url = _request.url;
+        if (url != "/favicon.ico") {
+            let urlSection = Url.parse(url).search.substr(1);
+            let childHTML = "<br>";
+            for (let i = 0; i < urlSection.length; i++) {
+                if (urlSection[i] == "&") {
+                    articles.push(childHTML);
+                    childHTML = "<br>";
+                }
+                else {
+                    childHTML += urlSection[i];
+                }
+            }
+            articles.push(childHTML);
+            for (let i = 0; i < articles.length; i++) {
+                _response.write(articles[i]);
+            }
+            console.log(articles);
+        }
+        _response.write(_request.url);
+        _response.end();
     }
 })(L06_SendData || (L06_SendData = {}));
 //# sourceMappingURL=Server.js.map
--- Aufgabe6/Server/Server.ts
+++ Aufgabe6/Server/Server.ts
@@ -1,33 +1,47 @@
-//Import die gesamte node.d.ts Datei und greift auf "declare module "http"" zu
 import * as Http from "http";
+import * as Url from "url";
+namespace L06_SendData {
+    console.log("Starting server"); 
+    let port: number = process.env.PORT; 
+    if (port == undefined) 
+         port = 8100;   
 
-namespace L06_SendData { //Funktionen werden abgegrenzt um Konflikte mit ähnlichen definierten Variablen im Progrmmcode zu vermeiden
-//Konsole gibt "Starting server" aus
-    console.log("Starting server");
-//process bezieht sich auf NodeJS.Heroku ist die angegebene Umgebung. Wenn Port nicht angegeben ist, wird Port 8100 benutzt 
-    let port: number = process.env.PORT;
-    if (port == undefined)
-        port = 8100;
-//Erlaubt den Transfer Hyper Text Transfer Protocol. 
-    let server: Http.Server = Http.createServer(); //Der PC fungiert als Server.
-    server.addListener("request", handleRequest); //Kommunikation zwischen Client und Server. Der Server sendet eine Anfrage "handleRequest".
-    server.addListener("listening", handleListen); //Listener wartet auf eingehede Daten
-    server.listen(port); //Hört auf den Port 8100 
+    let server: Http.Server = Http.createServer(); 
+    server.addListener("request", handleRequest); 
+    server.addListener("listening", handleListen); 
+    server.listen(port); 
+    
 
     function handleListen(): void {
-        console.log("Listening"); //Konsole gibt "Listening" aus
+        console.log("Listening"); 
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //Parameter Http.IncomingMessage bezieht sich auf Node.d.ts
-        console.log(_request.url); //Konsole gibt "I hear voices!" aus //Gibt die Url zurück
- //Verändert die Werte von setHeader
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Bestimmt den Zeichencode sowie das folgendes Dokument ein Text ist 
-        _response.setHeader("Access-Control-Allow-Origin", "*"); //Schützt Skriptsprachen-zugriff von fremden Quellen und erlaubt nur die eigene Ressource zu laden (Origin wäre das Protokoll, sowie die Domäne und der Port z.B https://eia2-nodetest.herokuapp.com mit dem Port 8100)  
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { 
+        console.log(_request.url); 
+        _response.setHeader("content-type", "text/html; charset=utf-8"); 
+        _response.setHeader("Access-Control-Allow-Origin", "*"); 
+        let articles: string[] = [];
+        let url: string = _request.url;
+        if (url != "/favicon.ico") {
+            let urlSection: string = Url.parse(url).search.substr(1);
+            let childHTML: string = "<br>";
+            for (let i: number = 0; i < urlSection.length; i++) {
+                if (urlSection[i] == "&") {
+                    articles.push(childHTML);
+                    childHTML = "<br>";
+                }
+                else {
+                    childHTML += urlSection[i];
+                }
+            }
+            articles.push(childHTML);
 
-        _response.write(_request.url); // Sendet eine Antwort an die angefragte URL  (
-       
-        _response.end();  //Der Inhalt von Response wird vom Client gesendet und es wird signalisiert das jene Anfrage vollständig gesendet wurde
-    //Server wird mit strg+c beendet
+            for (let i: number = 0; i < articles.length; i++) {
+                _response.write(articles[i]);
+            }
+            console.log(articles);
+        }
+        _response.write(_request.url); 
+        _response.end(); 
     }
-    
 }

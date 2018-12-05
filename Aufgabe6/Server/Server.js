5c7861251da1141c707a1ef2920f3e85d796d8d4
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-12-05 21:37:28 +0100

    test2

--- Aufgabe6/Server/Server.js
+++ Aufgabe6/Server/Server.js
@@ -1,45 +1,55 @@
 "use strict";
-const Http = require("http");
+const Http = require("http"); //importiert die node.d.ts Datei. Durch "Http" kann man nun auf das Modul "http" zugreifen (Zeile 615 in node.d.ts)
 const Url = require("url");
-var L06_SendData;
-(function (L06_SendData) {
+var A6;
+(function (A6) {
     console.log("Starting server");
-    let port = process.env.PORT;
+    let port = process.env.PORT; //process Bezieht sich auf NodeJS, falls nicht definiert nimmt er port = 8100. Dieser verweist auf https://eia2-nodetest.herokuapp.com
     if (port == undefined)
         port = 8100;
-    let server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+    let HTMLArray = [];
+    let server = Http.createServer(); //Erlaubt den http Transfer - fungiert als Server
+    server.addListener("request", handleRequest); //F�gt dem Server einen Listener zu. Wenn der Nutzer darauf zugreifen will wird handleRequest ausgef�hrt
+    server.addListener("listening", handleListen); //F�gt dem Server einen Listener zu. Solange der Nutzer auf den Server zugreif wird handleListen ausgef�hrt.
+    server.listen(port); //Spezifiziert, dass der Server auf den Port 8100 h�rt
     function handleListen() {
-        console.log("Listening");
+        console.log("Listening"); //Gib "Listening" in der Console aus
     }
     function handleRequest(_request, _response) {
-        console.log(_request.url);
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        let articles = [];
-        let url = _request.url;
-        if (url != "/favicon.ico") {
-            let urlSection = Url.parse(url).search.substr(1);
-            let childHTML = "<br>";
-            for (let i = 0; i < urlSection.length; i++) {
-                if (urlSection[i] == "&") {
-                    articles.push(childHTML);
-                    childHTML = "<br>";
+        console.log(_request.url); //Gib den eingegebenen Text in der Konsole aus
+        console.log("request");
+        //console.log("URLSearch:" + url);
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Ver�ndert die Werte des Serverheaders: name="content-type" und value="text/html; charset=utf-8"
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //Ver�ndert die Werte des Serverheaders: name="Access-Control-Allow-Origin" und value="*"
+        if (_request.url != "/favicon.ico") {
+            let url = Url.parse(_request.url).search.substr(1);
+            let HTML = "<br>";
+            for (let i = 0; i < url.length; i++) {
+                if (url[i] == "&") {
+                    HTMLArray.push(HTML);
+                    HTML = "<br>";
                 }
                 else {
-                    childHTML += urlSection[i];
+                    if (HTML == "<br>Text") {
+                        HTML = "<br>Adresse";
+                    }
+                    if (HTML == "<br>Pattern") {
+                        HTML = "<br>Hausnummer";
+                    }
+                    HTML += url[i];
+                    console.log(HTML);
                 }
             }
-            articles.push(childHTML);
-            for (let i = 0; i < articles.length; i++) {
-                _response.write(articles[i]);
+            HTMLArray.push(HTML);
+            HTMLArray.push("<br>");
+            HTMLArray.push("------------------");
+            HTMLArray.push("<br>");
+            for (let i = 0; i < HTMLArray.length; i++) {
+                _response.write(HTMLArray[i]);
             }
-            console.log(articles);
+            console.log(HTMLArray);
         }
-        _response.write(_request.url);
-        _response.end();
-    }
-})(L06_SendData || (L06_SendData = {}));
+        _response.end(); //response wird beendet. Dieser Aufruf muss immer bei einem response get�tigt werden
+    } //Strg + C zum beenden
+})(A6 || (A6 = {}));
 //# sourceMappingURL=Server.js.map
--- Aufgabe6/Server/Server.ts
+++ Aufgabe6/Server/Server.ts
@@ -1,47 +1,62 @@
-import * as Http from "http";
-import * as Url from "url";
-namespace L06_SendData {
-    console.log("Starting server"); 
-    let port: number = process.env.PORT; 
-    if (port == undefined) 
-         port = 8100;   
 
-    let server: Http.Server = Http.createServer(); 
-    server.addListener("request", handleRequest); 
-    server.addListener("listening", handleListen); 
-    server.listen(port); 
+import * as Http from "http"; //importiert die node.d.ts Datei. Durch "Http" kann man nun auf das Modul "http" zugreifen (Zeile 615 in node.d.ts)
+import * as Url from "url";
+
+namespace A6 {
+    console.log("Starting server");
+    let port: number = process.env.PORT; //process Bezieht sich auf NodeJS, falls nicht definiert nimmt er port = 8100. Dieser verweist auf https://eia2-nodetest.herokuapp.com
+    if (port == undefined)
+        port = 8100;
     
+    
+    let HTMLArray: string[] = [];
+    
+    let server: Http.Server = Http.createServer(); //Erlaubt den http Transfer - fungiert als Server
+    server.addListener("request", handleRequest); //Fügt dem Server einen Listener zu. Wenn der Nutzer darauf zugreifen will wird handleRequest ausgeführt
+    server.addListener("listening", handleListen); //Fügt dem Server einen Listener zu. Solange der Nutzer auf den Server zugreif wird handleListen ausgeführt.
+    server.listen(port); //Spezifiziert, dass der Server auf den Port 8100 hört
 
     function handleListen(): void {
-        console.log("Listening"); 
+        console.log("Listening"); //Gib "Listening" in der Console aus
     }
-
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { 
-        console.log(_request.url); 
-        _response.setHeader("content-type", "text/html; charset=utf-8"); 
-        _response.setHeader("Access-Control-Allow-Origin", "*"); 
-        let articles: string[] = [];
-        let url: string = _request.url;
-        if (url != "/favicon.ico") {
-            let urlSection: string = Url.parse(url).search.substr(1);
-            let childHTML: string = "<br>";
-            for (let i: number = 0; i < urlSection.length; i++) {
-                if (urlSection[i] == "&") {
-                    articles.push(childHTML);
-                    childHTML = "<br>";
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //übergibt server IncomingMessage als _request und server ServerResponse als _response
+        console.log(_request.url); //Gib den eingegebenen Text in der Konsole aus
+        console.log("request");
+        //console.log("URLSearch:" + url);
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Verändert die Werte des Serverheaders: name="content-type" und value="text/html; charset=utf-8"
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //Verändert die Werte des Serverheaders: name="Access-Control-Allow-Origin" und value="*"
+        if (_request.url != "/favicon.ico") {
+            let url: string = Url.parse(_request.url).search.substr(1);
+            let HTML: string = "<br>";
+            for (let i: number = 0; i < url.length; i++) {
+                if (url[i] == "&") {
+                    HTMLArray.push(HTML);
+                    HTML = "<br>";
                 }
                 else {
-                    childHTML += urlSection[i];
+                    if (HTML == "<br>Text") {
+                        HTML = "<br>Adresse"
+                        }
+                    if (HTML == "<br>Pattern") {
+                        HTML = "<br>Hausnummer"
+                        }
+                    HTML += url[i];
+                    console.log(HTML);
                 }
+                
+                
             }
-            articles.push(childHTML);
-
-            for (let i: number = 0; i < articles.length; i++) {
-                _response.write(articles[i]);
-            }
-            console.log(articles);
+            HTMLArray.push(HTML);
+            HTMLArray.push("<br>");
+            HTMLArray.push("------------------");
+            HTMLArray.push("<br>");
+            for (let i: number = 0 ; i < HTMLArray.length ; i++) {
+                _response.write(HTMLArray[i]);
+                }
+            console.log(HTMLArray);
+            //  _response.write(url);
         }
-        _response.write(_request.url); 
-        _response.end(); 
-    }
+
+        _response.end(); //response wird beendet. Dieser Aufruf muss immer bei einem response getätigt werden
+    } //Strg + C zum beenden
 }

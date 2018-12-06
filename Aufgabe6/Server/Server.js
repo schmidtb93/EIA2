fd2812d42192c03aae3ff6ce956c53c04d4aad79
schmidtb93 <43846720+schmidtb93@users.noreply.github.com>
2018-12-06 08:41:14 +0100

    test6

--- Aufgabe6/Server/Server.js
+++ Aufgabe6/Server/Server.js
@@ -1,17 +1,14 @@
 "use strict";
 //Import die gesamte node.d.ts Datei und greift auf "declare module "http"" zu
 const Http = require("http");
-const Url = require("url");
-var aufgabe6;
-(function (aufgabe6) {
+var L06_SendData;
+(function (L06_SendData) {
     //Konsole gibt "Starting server" aus
     console.log("Starting server");
     //process bezieht sich auf NodeJS.Heroku ist die angegebene Umgebung. Wenn Port nicht angegeben ist, wird Port 8100 benutzt 
     let port = process.env.PORT;
     if (port == undefined)
         port = 8100;
-    //leeres Array f�r die Artikel
-    let alleArtikel = [];
     //Erlaubt den Transfer Hyper Text Transfer Protocol. 
     let server = Http.createServer(); //Der PC fungiert als Server.
     server.addListener("request", handleRequest); //Kommunikation zwischen Client und Server. Der Server sendet eine Anfrage "handleRequest".
@@ -22,32 +19,12 @@ var aufgabe6;
     }
     function handleRequest(_request, _response) {
         console.log(_request.url); //Konsole gibt "I hear voices!" aus //Gibt die Url zur�ck
-        console.log("request");
+        //Ver�ndert die Werte von setHeader
         _response.setHeader("content-type", "text/html; charset=utf-8"); //Bestimmt den Zeichencode sowie das folgendes Dokument ein Text ist 
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Sch�tzt Skriptsprachen-zugriff von fremden Quellen und erlaubt nur die eigene Ressource zu laden (Origin w�re das Protokoll, sowie die Dom�ne und der Port z.B https://eia2-nodetest.herokuapp.com mit dem Port 8100)  
-        if (_request.url != "/favicon.ico") {
-            let urlEncoding = Url.parse(_request.url).search.substr(1);
-            let childHTML = "<p>";
-            for (let i = 0; i < urlEncoding.length; i++) {
-                if (urlEncoding[i] == "&") {
-                    alleArtikel.push(childHTML);
-                    childHTML = "<p>";
-                }
-                else {
-                    if (childHTML == "<p>Text") {
-                        childHTML = "<p>Adresse";
-                    }
-                    childHTML += urlEncoding[i];
-                }
-            }
-            alleArtikel.push(childHTML);
-            for (let i = 0; i < alleArtikel.length; i++) {
-                _response.write(alleArtikel[i]); // Sendet eine Antwort an die angefragte URL
-            }
-            console.log(alleArtikel);
-        }
+        _response.write(_request.url); // Sendet eine Antwort an die angefragte URL  (
         _response.end(); //Der Inhalt von Response wird vom Client gesendet und es wird signalisiert das jene Anfrage vollst�ndig gesendet wurde
         //Server wird mit strg+c beendet
     }
-})(aufgabe6 || (aufgabe6 = {}));
+})(L06_SendData || (L06_SendData = {}));
 //# sourceMappingURL=Server.js.map

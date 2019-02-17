445768f66bd84ee6e338629b93884c4730e5ce0b
Sebastian <Sebastian@SebastianLaptop>
2019-02-17 14:53:57 +0100

    Test2

--- Endabgabe/Classes/Movement.ts
+++
@@ -1,9 +0,0 @@
-namespace endabgabe {
-    export class Move extends Scenery {
-        dx: number;
-        dy: number;
-
-    move(): void {};
-    draw(): void {};
-        }
-}
--- Endabgabe/Classes/Objects.ts
+++
@@ -1,9 +0,0 @@
-namespace endabgabe {
-    export class Scenery {
-        x: number;
-        y: number;
-        
-         move(): void{};
-        draw(): void{};
-    }
-}
